9d5c7c6b7b3f5f22bb7195231139ee1234fe876f
Leandro Nascimento Nogueira <LeandroNascimentoNogueira@gmail.com>
2025-03-04 16:12:14 -0300

    first commit

---
+++ src/test/java/leandro/dev/encurtador/EncurtadorApplicationTests.java
@@ -0,0 +1,13 @@
+package leandro.dev.encurtador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
